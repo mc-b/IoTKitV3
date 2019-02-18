ae396fc4698d12a246adb0b0ac2b360c54ed5921
mc-b <marcel.bernet@ch-open.ch>
2019-02-18 20:19:16 +0100

    Fehlerhafte Library entfernt

--- actors/Schrittmotor/src/main.cpp
+++ actors/Schrittmotor/src/main.cpp
@@ -1,29 +1,58 @@
 /** Schrittmotor Beispiel 
-    Schrittmotor am oberen Stecker mit rotem Kabel nach unten einstecken.
+    Schrittmotor an den mit STEPPER1 und STEPPER2 Steckern mit rotem Kabel nach + einstecken.
 */
 #include "mbed.h"
-#include "StepperMotorUni.h"
 
-StepperMotorUni motor1( PTB20, PTB21, PTB1, PTB0 );
-StepperMotorUni motor2( PTB18, PTB19, PTC8, PTC9 );
+// Stepper 1
+DigitalOut s1( PTB20 );
+DigitalOut s2( PTB21 );
+DigitalOut s3( PTB1 );
+DigitalOut s4( PTB0 );
+
+// Stepper 2
+DigitalOut s5( PTB18 );
+DigitalOut s6( PTB19 );
+DigitalOut s7( PTC8 );
+DigitalOut s8( PTC9 );
+
+static float s = 0.005f;
 
 int main()
 {
     // Motordrehzahl
     printf( "Schrittmotor Test\n" );
-    motor1.set_pps( 300 );
-    motor2.set_pps( 300 );
 
     while ( 1 ) 
     {
         printf( "vorwaerts\n" );
-        motor1.move_steps( 1024 );   // halbe Umdrehung (180°)
-        motor2.move_steps( -1024 );   // halbe Umdrehung (180°)
-        wait( 6.0 );
-
+        for ( int i = 0; i < 1024/4; i++ )
+        {
+            s1 = 1; s2 = 0; s3 = 0; s4 = 0;
+            s5 = 1; s6 = 0; s7 = 0; s8 = 0;
+            wait( s );
+            s1 = 0; s2 = 1;
+            s5 = 0; s6 = 1;
+            wait( s );
+            s2 = 0; s3 = 1;
+            s6 = 0; s7 = 1;
+            wait( s );
+            s3 = 0; s4 = 1;
+            s7 = 0; s8 = 1;
+            wait( s );
+        }
+        wait( 0.5f );
         printf( "rueckwaerts\n" );
-        motor1.move_steps( -1024 );
-        motor2.move_steps( 1024 );
-        wait( 6.0 );
+        for ( int i = 0; i < 1024/4; i++ )
+        {
+            s4 = 1; s3 = 0; s2 = 0; s1 = 0;
+            wait( s );
+            s4 = 0; s3 = 1;
+            wait( s );
+            s3 = 0; s2 = 1;
+            wait( s );
+            s2 = 0; s1 = 1;
+            wait( s );
+        }
+        wait( 0.5f );
     }
 }
