1ccdb06fad4f315bd86e29706cdce151fa2c70c6
mbern <mbern@padmcb2>
2019-04-17 18:09:36 +0200

    6D Ausgabe

--- i2c/LSM6DSL/src/main.cpp
+++ i2c/LSM6DSL/src/main.cpp
@@ -6,39 +6,84 @@
 OLEDDisplay oled( PTE26, PTE0, PTE1);
 
 static DevI2C devI2c(PTE0,PTE1);
-static LSM6DSLSensor acc_gyro(&devI2c,LSM6DSL_ACC_GYRO_I2C_ADDRESS_LOW); // low address
-
+static LSM6DSLSensor acc(&devI2c,LSM6DSL_ACC_GYRO_I2C_ADDRESS_LOW); // low address
 
 int main()
 {
     uint8_t id;
-    int32_t axes[3];
+    uint8_t xl = 0;
+    uint8_t xh = 0;
+    uint8_t yl = 0;
+    uint8_t yh = 0;
+    uint8_t zl = 0;
+    uint8_t zh = 0;
+    char report[256];
 
     oled.clear();
-    oled.printf( "Acc/Gyro Sensor\n" );
+    oled.printf( "6D Orientation\n" );
 
     /* Init all sensors with default params */
-    acc_gyro.init(NULL);
-    acc_gyro.enable_x();
-    acc_gyro.enable_g();
+    acc.init(NULL);
+    acc.enable_g();
+    acc.enable_x();
+    acc.enable_6d_orientation();
 
-    acc_gyro.read_id(&id);
+    acc.read_id(&id);
     printf("LSM6DSL accelerometer & gyroscope = 0x%X\r\n", id);
 
 
     while (true)
     {
         oled.clear();
-        oled.cursor( 1, 0 );
 
-        acc_gyro.get_x_axes(axes);
-        printf("LSM6DSL [acc/mg]:        %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
-        oled.printf( "a:%6d%6d%6d\n", axes[0], axes[1], axes[2] );
+        acc.get_6d_orientation_xl(&xl);
+        acc.get_6d_orientation_xh(&xh);
+        acc.get_6d_orientation_yl(&yl);
+        acc.get_6d_orientation_yh(&yh);
+        acc.get_6d_orientation_zl(&zl);
+        acc.get_6d_orientation_zh(&zh);
 
-        acc_gyro.get_g_axes(axes);
-        printf("LSM6DSL [gyro/mdps]:     %6ld, %6ld, %6ld\r\n", axes[0], axes[1], axes[2]);
-        oled.printf("g:%6d%6d%6d", axes[0], axes[1], axes[2]);
+        printf( "\nxl %d, xh %d, yl %d, yh %d, zl %d, zh %d\n", xl, xh, yl, yh, zl, zh );
 
-        wait( 2.0f );
+        if ( xh )
+        {
+          sprintf( report, "    _____________\n" \
+                           " * |_____________|\n" );
+        }
+
+        else if ( xl )
+		{
+		  sprintf( report, " _____________\n" \
+						   "|_____________| *\n" );
+		}
+        else if ( yh)
+		{
+		  sprintf( report, " _____________\n" \
+						   "|______v______|\n" );
+		}
+        else if ( yl )
+		{
+		  sprintf( report, " _____________\n" \
+						   "|______^______|\n" );
+		}
+        else if ( zh )
+        {
+          sprintf( report, " ______*______ \n" \
+                           "|_____________|\n" );
+        }
+
+        else if ( zl )
+        {
+          sprintf( report,  " ____________\n" \
+                            "|____________|\n" \
+                            "      *       \n" );
+        }
+        else
+        {
+          sprintf( report, "None of the 6D orientation axes is set in LSM6DSL - accelerometer.\r\n" );
+        }
+        oled.printf( report );
+        printf( report );
+        wait( 1.0f );
     }
 }
