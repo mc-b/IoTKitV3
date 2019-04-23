82b3b7f90328785629b42b5363ff63c86257b0df
mbern <mbern@padmcb2>
2019-04-23 09:22:03 +0200

    u.s.b.

--- gpio/AnalogIn/src/main.cpp
+++ gpio/AnalogIn/src/main.cpp
@@ -3,21 +3,19 @@
 */
 #include <mbed.h>
 
-AnalogIn poti( PTC0 );
-DigitalOut led1 ( D10 );
+AnalogIn hallSensor( MBED_CONF_IOTKIT_HALL_SENSOR );
+DigitalOut led1( MBED_CONF_IOTKIT_LED1 );
 
 int main()
 {
     while   ( 1 )
     {
-        printf("Hall %4.2f\n", poti.read() );
-        if  ( poti.read() < 0.5f )          // in Prozent
+        printf("Hall %4.2f\n", hallSensor.read() );
+        if  ( hallSensor.read() < 0.5f )          // in Prozent
             led1 = 1;
         else
             led1 = 0;
             
         wait ( 1.0 );   
-        //if  ( licht.read_u16() > 32767 )    // als short Ganzzahl
-        //    ....;   
     }     
 }
