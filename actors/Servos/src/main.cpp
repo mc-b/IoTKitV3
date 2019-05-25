70ef4158ab27a4930ff7942b7ac197d49cad23c8
mc-b <marcel.bernet@ch-open.ch>
2019-05-25 18:00:30 +0200

    Umbau Servo Library

--- actors/Servos/src/main.cpp
+++ actors/Servos/src/main.cpp
@@ -1,36 +1,30 @@
 #include "mbed.h"
+#include "Servo.h"
 
-
-PwmOut servo1( MBED_CONF_IOTKIT_SERVO1 );
-PwmOut servo2( MBED_CONF_IOTKIT_SERVO2 );
-PwmOut servo3( MBED_CONF_IOTKIT_SERVO3 );
-PwmOut servo4( MBED_CONF_IOTKIT_SERVO4 );
-
+Servo servo1( MBED_CONF_IOTKIT_SERVO1 );
+Servo servo2( MBED_CONF_IOTKIT_SERVO2 );
+Servo servo3( MBED_CONF_IOTKIT_SERVO3 );
+Servo servo4( MBED_CONF_IOTKIT_SERVO4 );
 
 int main()
 {
-    servo1.period(0.020);          // servo requires a 20ms period
-    servo2.period(0.020);          // servo requires a 20ms period
-    servo3.period(0.020);          // servo requires a 20ms period
-    servo4.period(0.020);          // servo requires a 20ms period
-    
     while (1) 
     {
-        for(float offset=0.0; offset<0.001; offset+=0.0001) 
+        for ( float offset = 0.0f; offset < 1.0f; offset += 0.05f )
         {
-            servo1.pulsewidth(0.001 + offset); // servo position determined by a pulsewidth between 1-2ms
-            servo2.pulsewidth(0.001 + offset); // servo position determined by a pulsewidth between 1-2ms
-            servo3.pulsewidth(0.001 + offset); // servo position determined by a pulsewidth between 1-2ms
-            servo4.pulsewidth(0.001 + offset); // servo position determined by a pulsewidth between 1-2ms
-            wait(0.25);
+            servo1.write( offset);
+            servo2.write( offset);
+            servo3.write( offset);
+            servo4.write( offset);
+            wait( 0.25f );
         }
-        for(float offset=0.001; offset>0.000; offset-=0.0001) 
+        for ( float offset = 1.0f; offset > 0.0f; offset -= 0.05f )
         {
-            servo1.pulsewidth(0.001 + offset); // servo position determined by a pulsewidth between 1-2ms
-            servo2.pulsewidth(0.001 + offset); // servo position determined by a pulsewidth between 1-2ms
-            servo3.pulsewidth(0.001 + offset); // servo position determined by a pulsewidth between 1-2ms
-            servo4.pulsewidth(0.001 + offset); // servo position determined by a pulsewidth between 1-2ms
-            wait(0.25);
+            servo1.write( offset);
+            servo2.write( offset);
+            servo3.write( offset);
+            servo4.write( offset);
+            wait( 0.25f );
         }        
     }
 }
